34e608b638ae035bf16c38f241611d3321ea3f12
SoniaChevli <you@example.com>
2018-04-10 18:50:19 +0000

    Remove builtins

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-    if (x != ord('q') and x != ord('e')):
-        print(chr(x), end='')
+for x in range(97, 123):
+    if (x != 113 and x != 101):
+        print("{:c}".format(x), end='')
