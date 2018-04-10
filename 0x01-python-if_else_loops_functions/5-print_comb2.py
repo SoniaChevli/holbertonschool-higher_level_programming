558e4467f227d846c9bce497303093581f13da95
SoniaChevli <you@example.com>
2018-04-10 18:59:57 +0000

    Add 0 to front of single digits

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if (x != 99):
+    if(x < 10):
+        print("0{:d}".format(x), end=', ')
+    elif (x != 99):
         print("{:d}".format(x), end=', ')
     else:
         print("{:d}".format(x))
