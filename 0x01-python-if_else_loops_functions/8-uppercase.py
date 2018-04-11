cb4effd0385d8372e39abd494defde4c76872bd9
SoniaChevli <you@example.com>
2018-04-11 01:10:35 +0000

    Remove if statment

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,5 @@ def uppercase(str):
         x = ord(str[i])
         if (x >= 97 and x <= 122):
             x = x - 32
-        if (i == len(str) - 1):
-            print('{:c}'.format(x))
-        else:
-            print('{:c}'.format(x), end='')
+        print('{:c}'.format(x), end='')
+    print('')
