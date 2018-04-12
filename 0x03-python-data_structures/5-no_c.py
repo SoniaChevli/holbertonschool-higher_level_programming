27f345aea6f891e0ab4b469262e591e7aff0ef35
SoniaChevli <you@example.com>
2018-04-12 14:44:26 +0000

    Fix spacing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,4 +4,4 @@ def no_c(my_string):
         for i, d in enumerate(my_string):
             if d == 'c' or d == 'C':
                 my_string = my_string[:i] + my_string[i + 1:]
-    return my_string
+        return my_string
