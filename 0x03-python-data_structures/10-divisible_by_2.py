4eaa0da65712cc093e4a918fb79cd2b7c7173411
SoniaChevli <you@example.com>
2018-04-12 12:46:32 +0000

    Remove enumerate in loop

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python3
 def divisible_by_2(my_list=[]):
     list_dup = []
-    for i, d in enumerate(my_list):
+    for d in my_list:
         if d % 2 == 0:
             list_dup.append(True)
         else:
