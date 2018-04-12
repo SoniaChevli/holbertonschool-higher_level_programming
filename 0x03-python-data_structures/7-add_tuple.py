58b25612c8e7095fdc1ced41eeba88f6aaaf9136
SoniaChevli <you@example.com>
2018-04-12 12:02:13 +0000

    create 7-add_tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a = list(tuple_a)
+    tuple_b = list(tuple_b)
+    for i in range(0, 2):
+        if len(tuple_a) < 2:
+            tuple_a.append(0)
+        if len(tuple_b) < 2:
+            tuple_b.append(0)
+    tuple_b = tuple(tuple_b)
+    tuple_a = tuple(tuple_a)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
