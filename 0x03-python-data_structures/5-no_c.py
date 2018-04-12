3e60685101db5b61a399a06cf5992db84b90e132
SoniaChevli <you@example.com>
2018-04-12 17:31:42 +0000

    Change for to while loop

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i, d in enumerate(my_string):
-        if d == 'c' or d == 'C':
-            my_string = my_string[:i] + my_string[i + 1:]
+    while ('c' in my_string):
+        i = my_string.index('c')
+        my_string = my_string[:i] + my_string[i + 1:]
+    while ('C' in my_string):
+        i = my_string.index('C')
+        my_string = my_string[:i] + my_string[i + 1:]
     return my_string
