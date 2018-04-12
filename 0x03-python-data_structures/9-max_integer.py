894e5223b94d897d12ad0df9911bc4a809ef043a
SoniaChevli <you@example.com>
2018-04-12 14:25:32 +0000

    Add or if list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     biggest_int = 0
-    if len(my_list) == 0:
+    if len(my_list) == 0 or my_list == []:
         return None
     for i, d in enumerate(my_list):
         if d > biggest_int:
