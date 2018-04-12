0001af59e8a121fa4c2105fa9d45fc58ba1c2b04
SoniaChevli <you@example.com>
2018-04-12 17:16:59 +0000

    remove if string is none

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is not None:
-        for i, d in enumerate(my_string):
-            if d == 'c' or d == 'C':
-                my_string = my_string[:i] + my_string[i + 1:]
-        return my_string
+    for i, d in enumerate(my_string):
+        if d == 'c' or d == 'C':
+            my_string = my_string[:i] + my_string[i + 1:]
+    return my_string
