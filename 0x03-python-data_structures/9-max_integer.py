1f35816d9457709ac169eb4bd866ba6d9a24316f
SoniaChevli <you@example.com>
2018-04-12 17:10:27 +0000

    Write using .sort

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest_int = 0
-    if len(my_list) == 0 or my_list == []:
+    if my_list == []:
         return None
-    for i, d in enumerate(my_list):
-        if d > biggest_int:
-            biggest_int = d
-    return biggest_int
+    my_list.sort()
+    return my_list[-1]
