0a8423faadf41588ac21b549f68ae8b837190087
SoniaChevli <you@example.com>
2018-04-12 11:28:33 +0000

    Create 6-print_matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, d in enumerate(row):
+            if i != len(row) - 1:
+                print('{:d}'.format(d), end=' ')
+            else:
+                print('{:d}'.format(d))
