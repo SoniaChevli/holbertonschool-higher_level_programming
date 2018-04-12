8dc63ef20b8b80a9ff5a628f3e1d134cc9204ab8
SoniaChevli <you@example.com>
2018-04-12 12:10:21 +0000

    Create 9-max_int

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    biggest_int = 0
+    if len(my_list) == 0:
+        return None
+    for i, d in enumerate(my_list):
+        if d > biggest_int:
+            biggest_int = d
+    return biggest_int
