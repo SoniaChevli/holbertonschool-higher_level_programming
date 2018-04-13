f34bb4586de6dc3724eafaaff4bb538377a1c403
SoniaChevli <you@example.com>
2018-04-13 12:20:28 +0000

    Check if dic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return
-    return max(a_dictionary)
+    if type(a_dictionary) is not dict:
+        return None
+    if a_dictionary is not None:
+        return max(a_dictionary)
