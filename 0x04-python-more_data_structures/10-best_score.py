72da9e6dc9ffe2c9538bcd3083464c46785b03f8
SoniaChevli <you@example.com>
2018-04-13 16:33:31 +0000

    Check if dic empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if type(a_dictionary) is not dict:
         return None
+    if a_dictionary == {}:
+        return None
     if a_dictionary is not None:
         return max(a_dictionary)
