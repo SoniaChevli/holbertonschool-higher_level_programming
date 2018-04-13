7bc7a3bf4a55ff6f5af6bbefea13a73f8bd780b2
SoniaChevli <you@example.com>
2018-04-13 08:34:10 +0000

    Make pep8 style

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list =[]
+    new_list = []
     for d in my_list:
         if d == search:
             new_list.append(replace)
