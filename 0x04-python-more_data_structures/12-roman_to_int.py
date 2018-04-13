e85107221018a74ca88934a7b5469c687577f176
SoniaChevli <you@example.com>
2018-04-13 11:12:31 +0000

    Check if string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -20,16 +20,17 @@ def translate_letter(roman_string):
 
 
 def roman_to_int(roman_string):
-    number = 0
-    roman_string = translate_letter(roman_string)
-    for i, d in enumerate(roman_string):
-        i = i + 1
-        j = d
-        i = i - 1
-        if d < j:
-            number = number + (j - d)
+    if type(roman_string) is str:
+        number = 0
+        roman_string = translate_letter(roman_string)
+        for i, d in enumerate(roman_string):
             i = i + 1
-        else:
-            number = number + d
+            j = d
+            i = i - 1
+            if d < j:
+                number = number + (j - d)
+                i = i + 1
+            else:
+                number = number + d
 
-    return number
+                return number
