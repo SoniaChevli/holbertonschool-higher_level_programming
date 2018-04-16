813277d7a0201490ed49bb76f614c11e93c17395
Sonia Chevli <soniachevli@gmail.com>
2018-04-16 13:47:42 +0000

    fix typo in err message

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,7 @@ def list_division(my_list_1, my_list_2, list_length):
             divi = 0
         except IndexError:
             divi = 0
-            print("out fo range")
+            print("out of range")
         finally:
             new_list.append(divi)
     return new_list
