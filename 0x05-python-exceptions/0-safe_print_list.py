a13b00d963d3625fc09e699219436b3db9c04ee2
Sonia Chevli <soniachevli@gmail.com>
2018-04-16 09:30:31 +0000

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    j = 0
+
+    try:
+        for i in range(x):
+            print('{}'.format(my_list[i]), end='')
+            j += 1
+    except IndexError:
+        print('')
+        return j
+    else:
+        print('')
+        return j
