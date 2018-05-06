1e83f6040582a41b2c02df1222e72681fb78c6b4
Sonia Chevli <soniachevli@gmail.com>
2018-05-06 10:38:38 +0000

    Create 6-max int

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """ Tests for 6-max_integer.py"""
+
+    def setUp(self):
+        pass
+
+    def test_10_is_max(self):
+        """is 10 the max integer?"""
+        self.assertEqual(max_integer([1, 2, 3, 10]), 10)
+
+    def test_len_0(self):
+        """is the length of matrix 0"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_negative(self):
+        """how it reacts to negative numbers"""
+        self.assertEqual(max_integer([3, -2, -1, -100]), 3)
+
+    def test_not_matrix(self):
+        """no matrix given"""
+        self.assertEqual(max_integer(3), 3)
+
+    def test_double_matrix(self):
+        """ given double matrix"""
+        self.assertEqual(max_integer([[2, 3], [2, 4]]), [2, 4])
+
+    def test_matrix_letters(self):
+        """ only letters in matrix """
+        self.assertEqual(max_integer(["A", "B", "C"]), 'C')
+
+
+if __name__ == '__main__':
+    unittest.main()
