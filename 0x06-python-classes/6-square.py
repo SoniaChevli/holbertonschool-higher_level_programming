b3576150fd3869d90b0b193c9f2943fa2d82ac39
Sonia Chevli <soniachevli@gmail.com>
2018-05-08 05:01:35 +0000

    Change and to or

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,9 +24,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) and len(value) == 2 and \
-           isinstance(value[0], int) and instance(value[1], int) \
-           and value[0] >= 0 and value[1] >= 0:
+        if not isinstance(value, tuple) or len(value) == 2 or \
+           isinstance(value[0], int) or instance(value[1], int) \
+           and value[0] >= 0 or value[1] >= 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
