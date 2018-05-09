85b28cea08a319ff6c2fbe91ba3a01bc82e80b76
Sonia Chevli <soniachevli@gmail.com>
2018-05-09 08:39:08 +0000

    Create 11-square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ area """
+        return super().area()
+
+    def __str__(self):
+        return '[square] {:d}/{:d}'.format(self.__size, self.__size)
