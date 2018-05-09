b124115a1ae4eac165f60cf74247c293fd9185c1
Sonia Chevli <soniachevli@gmail.com>
2018-05-09 21:56:48 +0000

    add validator

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,4 +6,5 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """ square class"""
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
