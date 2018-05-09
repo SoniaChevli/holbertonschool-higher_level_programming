d7ae337a9abfebf1b0b550a029615c5ef2395a7c
Sonia Chevli <soniachevli@gmail.com>
2018-05-09 19:01:01 +0000

    Create 12-student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+class Student:
+    """ class defined as student"""
+    def __init__(self, first_name, last_name, age):
+        """instantiation of square class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ gives all the attibutes"""
+        at = {}
+        if isinstance(attrs, list):
+            for key in attrs:
+                if key in self.__dict__:
+                    at[key] = self.__dict__[key]
+            return at
+        else:
+            return self.__dict__
