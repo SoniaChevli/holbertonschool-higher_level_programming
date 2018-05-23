790e0995300272244de549ce490873d919850af9
Sonia Chevli <soniachevli@gmail.com>
2018-05-23 02:15:16 +0000

    fix pep8 style

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,10 +18,13 @@ class Square(Rectangle):
         Nothing
         """
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """ overload str method """
-        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(self.id, self.x, self.y, self.width)
+        return '[Square] ({:d}) {:d}/{:d} - {:d}'.format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
 
     @property
     def size(self):
@@ -48,11 +51,19 @@ class Square(Rectangle):
             if argnum >= 1:
                 self.id = args[0]
             if argnum >= 2:
-                self.__size = args[1]
+                self.size = args[1]
             if argnum >= 3:
-                self.__x = args[2]
+                self.x = args[2]
             if argnum >= 4:
-                self.__y = args[3]
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ gives the dictionary representation of Rectangle"""
+        final = {}
+        attrib = ['size', 'x', 'y', 'id']
+        for i in attrib:
+            final[i] = getattr(self, i)
+        return final
