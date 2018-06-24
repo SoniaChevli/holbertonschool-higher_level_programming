427bdf9f6421fd8b13b81c1c7263263c8192043e
Sonia Chevli <soniachevli@gmail.com>
2018-06-24 07:46:00 +0000

    capitalize keyword

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California found in hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id from states WHERE name='California') ORDER BY cities.id ASC;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name='California') ORDER BY cities.id ASC;
