ee593df5ed5f0629fd3bc9405ecf8e45b3e30412
Sonia Chevli <soniachevli@gmail.com>
2018-07-19 20:55:06 +0000

    frist commit

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+lists all cities of the state passed in
+"""
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3])
+
+    cur = connection.cursor()
+    cmd = "SELECT cities.name FROM states \
+    JOIN cities ON states.id = cities.state_id \
+    WHERE states.name = '{}'".format(sys.argv[4])
+    citynum = cur.execute(cmd)
+    cities = cur.fetchall()
+    for i in range(citynum):
+        if i == citynum - 1:
+            print(cities[i][0])
+        else:
+            print(cities[i][0], end=", ")
+
+    cur.close()
+    connection.close
+    sys.exit()
