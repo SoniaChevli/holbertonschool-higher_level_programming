cde013dabaca3083794d98599173b746257b4654
Sonia Chevli <soniachevli@gmail.com>
2018-07-19 19:48:31 +0000

    change to .format

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == '__main__':
                                  db=sys.argv[3])
 
     cur = connection.cursor()
-    cmd = "SELECT * FROM states WHERE name = '%s' ORDER BY id ASC"\
-          % (sys.argv[4])
+    cmd = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
+          .format(sys.argv[4]))
     cur.execute(cmd)
     states = cur.fetchall()
 
