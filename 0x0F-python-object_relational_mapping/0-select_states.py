21335692079d49b1927ae89d7d7b088e2da241d8
Sonia Chevli <soniachevli@gmail.com>
2018-07-19 19:25:03 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" lists all states from hbtn_0e_0_usa """
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    connection = MySQLdb.connect(host="localhost",
+                                 user=sys.argv[1],
+                                 passwd=sys.argv[2],
+                                 db=sys.argv[3])
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM states")
+    states = cur.fetchall()
+    for state in states:
+        print (state)
+
+    connection.close()
+    sys.exit()
