cfde7b99123c50c6db118c18f9c6867151fa50a4
Sonia Chevli <soniachevli@gmail.com>
2018-07-20 01:23:56 +0000

    change id to state_id

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     cur = connection.cursor()
 
     cur.execute("SELECT cities.id, cities.name, states.name \
-    FROM cities INNER JOIN states ON states.id = cities.id")
+    FROM cities JOIN states ON states.id = cities.state_id \
+    ORDER BY cities.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
