225500a47da5e310e235a8f87c0a49d29db9d62f
Sonia Chevli <soniachevli@gmail.com>
2018-07-20 01:57:41 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prints the first State object from the database
+"""
+if __name__ == '__main__':
+    from sqlalchemy import create_engine, MetaData
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    connection = engine.connect()
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).\
+             filter(State.name.contains('a')).order_by(State.id)
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
