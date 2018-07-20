b9e2af2cb0e9850e5fcc400400494e4c775ec2ee
Sonia Chevli <soniachevli@gmail.com>
2018-07-20 00:27:21 +0000

    add charset

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,9 +5,11 @@ if __name__ == '__main__':
     import sys
 
     connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
                                  user=sys.argv[1],
                                  passwd=sys.argv[2],
-                                 db=sys.argv[3])
+                                 db=sys.argv[3],
+                                 charset="utf8")
     cur = connection.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     states = cur.fetchall()
@@ -15,4 +17,5 @@ if __name__ == '__main__':
         print (state)
 
     connection.close()
+    cur.close()
     sys.exit()
