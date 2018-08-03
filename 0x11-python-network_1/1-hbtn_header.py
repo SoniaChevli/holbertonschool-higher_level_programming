b598fec6c50e9f402ba9c0eace16dc9d6d067a22
Sonia Chevli <soniachevli@gmail.com>
2018-08-03 16:23:20 +0000

    check if name == main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ and displays the value of the X-Request-Id
 '''
 import urllib.request
 import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-    headers = response.info()
-    print(headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        headers = response.info()
+        print(headers['X-Request-Id'])
