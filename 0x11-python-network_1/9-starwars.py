0dfba42e31e9434041cb71832ab79322776f3855
Sonia Chevli <soniachevli@gmail.com>
2018-08-03 20:01:37 +0000

    first commit

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+takes in a string
+sends a search request to the Star Wars API
+'''
+import requests
+import sys
+
+r = requests.get('https://swapi.co/api/people/',
+                 params={'search': sys.argv[1]})
+x = r.json()
+print('Number of results: {}'.format(x['count']))
+for i in x['results']:
+    print(i['name'])
