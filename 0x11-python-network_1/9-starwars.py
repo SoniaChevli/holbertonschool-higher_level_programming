5e5d56df5dbe8810b4b33c4f245fb0693954e9a6
Sonia Chevli <soniachevli@gmail.com>
2018-08-03 20:02:26 +0000

    check if name is main

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -5,10 +5,10 @@ sends a search request to the Star Wars API
 '''
 import requests
 import sys
-
-r = requests.get('https://swapi.co/api/people/',
-                 params={'search': sys.argv[1]})
-x = r.json()
-print('Number of results: {}'.format(x['count']))
-for i in x['results']:
-    print(i['name'])
+if __name__ == '__main__':
+    r = requests.get('https://swapi.co/api/people/',
+                     params={'search': sys.argv[1]})
+    x = r.json()
+    print('Number of results: {}'.format(x['count']))
+    for i in x['results']:
+        print(i['name'])
