f53e3f3cdc1a4a53fa52d13bd050de812ec74051
Sonia Chevli <soniachevli@gmail.com>
2018-08-03 15:40:44 +0000

    first commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,12 @@ fetches a url
 import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    utf8_content = response.read().decode('utf-8')
+    str1 = ''
     for line in response:
-        print (line)
+        str1 += line
+
+    print('Body response:')
+    print('\t- type: <class \'bytes\'>')
+    print('\t- content: %s'.format(str1))
+    print('\t- utf8 content: %s'.format(utf8_content))
