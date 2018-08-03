5920597050c95245a9d37b507777b668bfd963bd
Sonia Chevli <soniachevli@gmail.com>
2018-08-03 16:52:47 +0000

    put data in if statement

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,11 +7,10 @@ argv[2]: email
 import urllib.request
 import sys
 import urllib.parse
-
-data = {
-    'email': sys.argv[2]
-}
 if __name__ == '__main__':
+    data = {
+        'email': sys.argv[2]
+    }
     data = urllib.parse.urlencode({'email': sys.argv[2]}).encode("utf-8")
     with urllib.request.urlopen(sys.argv[1], data) as response:
         print(response.read())
