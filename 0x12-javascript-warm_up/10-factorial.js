a52c60f890fb0085babe91167546ba98460f06d0
Sonia Chevli <soniachevli@gmail.com>
2018-08-28 15:20:27 +0000

    change from arrow function to regular

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-let factorial = (i, total = 1) =>
+function factorial (i, total = 1) {
   i > 1 ? factorial(i - 1, total * i) : console.log(total);
+}
 
 factorial(parseInt(process.argv[2]));
