7c6ce691d46d5ea18b466e460c9ef3748aac7033
Sonia Chevli <soniachevli@gmail.com>
2018-08-28 15:11:27 +0000

    semistandard style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-let arr = ["C is fun", "Python is cool", "Javascript is amazing"];
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
 for (i in arr) console.log(arr[i]);
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 let size = parseInt(process.argv[2]);
-if (!size) console.log("Missing size");
-else for (let i = size; i > 0; i--) console.log("X".repeat(size));
+if (!size) console.log('Missing size');
+else for (let i = size; i > 0; i--) console.log('X'.repeat(size));
