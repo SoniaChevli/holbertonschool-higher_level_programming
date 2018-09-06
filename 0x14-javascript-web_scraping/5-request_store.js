f75ef82dde3576f5bbd3e8aa77a83bd99a174c34
Sonia Chevli <soniachevli@gmail.com>
2018-09-06 17:19:45 +0000

    add error message

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,6 +4,6 @@ let fs = require('fs');
 let finalStorage = process.argv[3];
 let URL = process.argv[2];
 
-request(URL, function (error, response, body) {}).pipe(
+request(URL, function (error, response, body) { if (error) console.log(error); }).pipe(
   fs.createWriteStream(finalStorage)
 );
